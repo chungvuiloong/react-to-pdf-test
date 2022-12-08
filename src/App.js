6325d8f78fd13e2cc42d1d35783f748e87e791c1
Jere <35882808+jirimicvl@users.noreply.github.com>
2022-12-08 10:10:56 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import Pdf from "react-to-pdf";
+
+const ref = React.createRef();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Pdf targetRef={ref} filename="code-example.pdf">
+        {
+          ({ toPdf }) => <button onClick={toPdf}>Convert to Pdf</button>
+        }
+      </Pdf>
+      {/* <div ref={ref}>
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div> */}
+
+      {/* Testing another paragraph */}
+      <div ref={ref}>
+        <h1>Test test</h1>
+        <h2>Meow meow</h2>
+      </div>
     </div>
   );
 }
